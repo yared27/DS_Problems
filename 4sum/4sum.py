9527259e832d09cff6b8559e046bf9d1ae2707a7
Yared <alemayehuyared9@gmail.com>
2025-04-15 11:30:14 -0700

    Adding 4sum problem

---
+++ 4sum/4sum.py
@@ -0,0 +1,38 @@
+
+arr = []
+
+n= int(input("Enter the number of element"))
+
+print("Enter the element")
+for i in range(n):
+    arr.append(int(input()))
+target = int(input("Enter the target"))
+
+def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+    nums.sort()
+    res = []
+    n = len(nums)
+
+    for i in range(n):
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        for j in range(i + 1, n):
+            if j > i + 1 and nums[j] == nums[j - 1]:
+                continue
+            left, right = j + 1, n - 1
+            while left < right:
+                total = nums[i] + nums[j] + nums[left] + nums[right]
+                if total == target:
+                    res.append([nums[i], nums[j], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elif total < target:
+                    left += 1
+                else:
+                    right -= 1
+    return res
+fourSum(arr,target)
