1f96c17c44e7798f165473cdebb993324f4dd174
Yared <alemayehuyared9@gmail.com>
2025-06-09 07:20:27 -0700

    Remove unnecessary comments

--- A2SV Remote EducationContest/contest17/B_Sum_Duel.py
+++ A2SV Remote EducationContest/contest17/B_Sum_Duel.py
@@ -87,28 +87,3 @@ for _ in range(t):
 
 
 
-# import sys
-# from collections import Counter
-# input = sys.stdin.readline
-
-# t = int(input())
-# for _ in range(t):
-#     n, k = map(int, input().split())
-#     arr = list(map(int, input().split()))
-#     freq = Counter(arr)
-
-#     score = 0
-#     visited = set()
-
-#     for x in freq:
-#         y = k - x
-#         if y in freq and x not in visited and y not in visited:
-#             if x == y:
-#                 score += freq[x] // 2
-#             else:
-#                 score += min(freq[x], freq[y])
-#             visited.add(x)
-#             visited.add(y)
-
-#     # Score cannot exceed n//2 (total pairs possible)
-#     print(min(score, n // 2))
--- A2SV Remote EducationContest/contest17/C_Number_Mutation.py
+++ A2SV Remote EducationContest/contest17/C_Number_Mutation.py
@@ -125,25 +125,3 @@ else:
 
 
 
-# from collections import Counter
-
-# t = int(input())
-# for _ in range(t):
-#     n, k = map(int, input().split())
-#     s = list(map(int, input().split()))
-#     freq = Counter(s)
-#     score = 0
-
-#     for x in list(freq.keys()):
-#         y = k - x
-#         if x == y:
-#             # Pair (x, x) can be made floor(freq[x] / 2) times
-#             score += freq[x] // 2
-#             freq[x] = 0
-#         elif y in freq:
-#             pairs = min(freq[x], freq[y])
-#             score += pairs
-#             freq[x] -= pairs
-#             freq[y] -= pairs
-
-#     print(score)
