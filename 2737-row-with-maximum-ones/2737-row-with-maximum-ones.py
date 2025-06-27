652fc2ffcb0c39c4c486917e176ab5673c6d3d02
Yared Alemayehu <alemayehuyared9@gmail.com>
2025-06-27 10:32:47 -0700

    Time: 4 ms (88.29%), Space: 18.7 MB (20.98%) - LeetHub

--- 2737-row-with-maximum-ones/2737-row-with-maximum-ones.py
+++ 2737-row-with-maximum-ones/2737-row-with-maximum-ones.py
@@ -1,18 +1,11 @@
 class Solution:
     def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
-        index = -1
-        noOne =0
-        indexVal ={}
-        for i in range(len(mat)):
-            currentNumberOfOne = 0
-            for j in range(len(mat[0])):
-                if mat[i][j]==1:
-                    currentNumberOfOne+=1
-            if currentNumberOfOne in indexVal:
-                indexVal[currentNumberOfOne] = min(indexVal[currentNumberOfOne],i)
-            else:
-                indexVal[currentNumberOfOne]=i
-            noOne=max(noOne,currentNumberOfOne)
-        return [indexVal[noOne],noOne]
-
+        index = 0
+        maxOne =0
+        for i,row in enumerate(mat):
+            noOne =row.count(1)
+            if noOne>maxOne:
+                maxOne = noOne
+                index=i
+        return [index,maxOne]
 
