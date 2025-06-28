5e845688bb2628498cf395741ab1c226e5e778f7
Yared Alemayehu <alemayehuyared9@gmail.com>
2025-06-28 13:29:50 -0700

    Time: 0 ms (100%), Space: 17.6 MB (98.59%) - LeetHub

--- 2421-maximum-number-of-pairs-in-array/2421-maximum-number-of-pairs-in-array.py
+++ 2421-maximum-number-of-pairs-in-array/2421-maximum-number-of-pairs-in-array.py
@@ -4,9 +4,6 @@ class Solution:
         pairs=0
         odds=0
         for val,counts in count.items():
-            if counts%2==0:
-                pairs+=(counts)//2
-            else:
-                odds+=1
-                pairs+=(counts-1)//2
+            pairs+=counts//2
+            odds+=counts%2
         return [pairs,odds]
