6a347fe4c2c93c3d65d2d2384aa5efcb4aa302be
Yared Alemayehu <alemayehuyared9@gmail.com>
2025-07-07 00:17:14 -0700

    Time: 111 ms (86.46%), Space: 20.7 MB (50.76%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.py
@@ -0,0 +1,22 @@
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        n , m = len(mat), len(mat[0])
+        nearestZero = [[-1 for _ in range(m)]for _ in range(n)]
+        queue = deque()
+        for i in range(n):
+            for j in range(m):
+                if mat[i][j] == 0:
+                    nearestZero[i][j] = 0
+                    queue.append([i,j])
+        nearestDistance = 0
+        while queue:
+            for _ in range(len(queue)):
+                x,y = queue.popleft()
+                for nr , nc in [(0,1),(0,-1),(1,0),(-1,0)]:
+                    newRow, newCol = x+nr, y+nc
+                    if 0<= newRow < n and 0<= newCol < m and nearestZero[newRow][newCol]==-1:
+                            nearestZero[newRow][newCol] = nearestDistance+1
+                            queue.append([newRow, newCol])
+
+            nearestDistance+=1
+        return nearestZero
