bc343e80b97dc3b81f19213ffad0e564c4571090
Yared Alemayehu <alemayehuyared9@gmail.com>
2025-08-02 14:37:10 +0300

    Time: 39 ms (56.14%), Space: 30 MB (42.69%) - LeetHub

---
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.py
@@ -0,0 +1,17 @@
+from math import ceil
+from typing import List
+
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        operations = 0
+        maxAllowed = nums[-1]
+
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] > maxAllowed:
+                parts = ceil(nums[i] / maxAllowed)
+                operations += parts - 1
+                maxAllowed = nums[i] // parts
+            else:
+                maxAllowed = nums[i]
+
+        return operations
