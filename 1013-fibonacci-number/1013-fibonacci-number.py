27311540d35967559553851d37a09dbc85bd5186
Yared Alemayehu <alemayehuyared9@gmail.com>
2025-08-06 14:47:01 +0300

    Time: 33 ms (87.95%), Space: 17.9 MB (23.02%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,9 +1,13 @@
 class Solution:
-    def __init__(self):
-        self.memo = {0:0, 1:1}
+    # def __init__(self):
+    #     self.memo = {0:0, 1:1}
     def fib(self, n: int) -> int:
-        if n not in self.memo:
-            self.memo[n] = self.fib(n-1) + self.fib(n-2)
+        # if n not in self.memo:
+        #     self.memo[n] = self.fib(n-1) + self.fib(n-2)
         
-        return self.memo[n]
+        # return self.memo[n]
+        ans = [0,1]
+        for i in range(2,n+1):
+            ans.append(ans[i-1]+ans[i-2])
+        return ans[n]
        
