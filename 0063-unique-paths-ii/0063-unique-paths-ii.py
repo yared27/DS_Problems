0e2f8b96251e4785bc9ce2abe23183349d75ae9e
Yared Alemayehu <alemayehuyared9@gmail.com>
2025-08-10 21:07:15 +0300

    Time: 0 ms (100%), Space: 18 MB (48.75%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,20 +1,20 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0] * n for _ in range(m)]
-        
-        if obstacleGrid[m-1][n-1] == 0:
-            dp[m-1][n-1] = 1
-        
+        n, m = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0 for _ in range(m)] for _ in range(n)]
+
+        if obstacleGrid[n-1][m-1] == 0:
+            dp[n-1][m-1] = 1
+
         def uniquePath(i, j):
-            if i < m and j < n and obstacleGrid[i][j] != 1:
+            if i<n and j<m and obstacleGrid[i][j] !=1:
                 return dp[i][j]
             return 0
-        
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
-                if obstacleGrid[i][j] != 1:
-                    if not (i == m-1 and j == n-1):  # skip base case cell
-                        dp[i][j] = uniquePath(i+1, j) + uniquePath(i, j+1)
-        
+
+        for i in range(n-1, -1, -1):
+            for j in range(m-1, -1, -1):
+                if obstacleGrid[i][j]!=1:
+                    if not (i==n-1 and j==m-1):
+                        dp[i][j] = uniquePath(i+1,j) + uniquePath(i,j+1)
+                    
         return dp[0][0]
