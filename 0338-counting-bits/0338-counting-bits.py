35b27699fb70982f16231e56e3549ddd608824aa
Yared Alemayehu <alemayehuyared9@gmail.com>
2025-08-25 04:16:15 -0700

    Time: 7 ms (51.57%), Space: 18.5 MB (90.42%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,16 +1,9 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        output = []
 
-        def countOnes(num):
-            noOne = 0
-            while num:
-                if num & 1:
-                    noOne += 1
-                num >>= 1
-            return noOne
-        for i in range(n+1):
-            output.append(countOnes(i))
+        output = [0]
+        for i in range(1,n+1):
+            output.append(output[i>>1] + (i&1))
         
         return output
                   
