298937ec0943d91935f3d8d517922ed082faf0a3
Yared Alemayehu <alemayehuyared9@gmail.com>
2025-09-02 06:32:57 -0700

    Time: 15 ms (15.1%), Space: 18.7 MB (96.06%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,21 +1,41 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        provinces = 0
-        visited = set()
-        n=len(isConnected)
-        def dfs(city):
-            for neighbor in range(n):
-                if isConnected[city][neighbor]==1 and neighbor not in visited:
-                    visited.add(neighbor) 
-                    dfs(neighbor)
+        n = len(isConnected)
+        root = {i : i for i in range(n)}
+        rank = [0]*n
         
-        for  city in range(n):
-            if city not in visited:
-                dfs(city)
-                provinces+=1
-        return provinces
-                    
-                    
+        def find(x):
+            while x != root[x]:
+                root[x] = root[root[x]]
+                x = root[x]
+            return root[x]
+        
+        def union(x,y):
+            xroot = find(x)
+            yroot = find(y)
+
+            if xroot != yroot:
+                if rank[xroot] > rank[yroot]:
+                    root[yroot] = xroot
+                elif rank[yroot] > rank[xroot]:
+                    root[xroot] = yroot
+        
+                else:
+                    root[xroot] = yroot
+                    rank[yroot] += 1
+        def isConnectedCity(x,y):
+            return root[x] == root[y]
+        noProvinces = n
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] and find(i) != find(j):
+                    noProvinces -= 1
+                    union(i,j)
+        
+
+        return noProvinces
+
+        
 
 
 
