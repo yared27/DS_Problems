c416e83da227580b6123e3a9d5a0b8e6cd14bfb6
Yared Alemayehu <alemayehuyared9@gmail.com>
2025-10-13 05:11:44 -0700

    Time: 354 ms (98.95%), Space: 19.8 MB (33.11%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -2,21 +2,18 @@ class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         graph = {i:[] for i in range(1,n+1)}
 
-        for u, v, t in times:
-            graph[u].append((v,t))
+        for u, v, w in times:
+            graph[u].append((v,w))
+        times = [float('inf')]*(n+1)
+        times[k] = 0
+        heap = [(times[k],k)]
 
-        distances = [float("inf")]*(n+1)
-        distances[k] = 0
-        hp = [(0,k)]
-
-        while hp:
-            time, node = heapq.heappop(hp)
-            if time > distances[node]:
-                continue 
-            for child, childTime in graph[node]:
-                new_time = childTime + time
-                if distances[child] > new_time:
-                    distances[child] = new_time
-                    heapq.heappush(hp,(new_time, child))
-
-        return max(distances[1:]) if max(distances[1:]) != float("inf") else -1
+        while heap:
+            w, node = heapq.heappop(heap)
+            if w > times[node]:
+                continue
+            for n, wt in graph[node]:
+                if wt+w < times[n]:
+                    times[n] = wt+w
+                    heapq.heappush(heap, (times[n], n))
+        return max(times[1:]) if max(times[1:]) != float("inf") else -1
