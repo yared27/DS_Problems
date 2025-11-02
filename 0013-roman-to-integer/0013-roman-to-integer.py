3783f956c3b023db5d0bf87ee074bb7d8da5c2f7
Yared Alemayehu <alemayehuyared9@gmail.com>
2025-11-02 05:02:05 -0800

    Time: 4 ms (64.01%), Space: 18 MB (20.69%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,7 +2,6 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         roman = {'I':1, 'V':5, 'X':10, 'L':50,
                  'C':100, 'D':500, 'M':1000}
-        
         result = 0
         for i in range(len(s)):
             curr = roman[s[i]]
