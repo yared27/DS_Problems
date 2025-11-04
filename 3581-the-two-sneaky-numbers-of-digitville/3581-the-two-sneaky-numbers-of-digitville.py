aa011a8e5fda676002c4e8a19dad42bf5e7cec01
Yared Alemayehu <alemayehuyared9@gmail.com>
2025-11-04 11:41:12 -0800

    Time: 0 ms (100%), Space: 17.9 MB (40.74%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getSneakyNumbers(self, nums: List[int]) -> List[int]:
+        seen = set()
+        res = []
+        for num in nums:
+            if num in seen:
+                res.append(num)
+            else:
+                seen.add(num)
+        return res
